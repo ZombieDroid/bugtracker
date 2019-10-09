68b868b6f840335ad37237d35c9ec8697daf6960
ammolnar <ammolnar@ADMINRG-RSD2V12.home>
2019-10-09 22:40:06 +0200

    maven projekt létrehozása

---
+++ bugtracker/src/main/java/bugtracker/BugTrackerApp.java
@@ -0,0 +1,15 @@
+package bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BugTrackerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerApp.class, args);
+
+	}
+
+}
