42d26fb1f5c739ae3ba2b2718b21b5675e3abd4b
ZombieDroid <sza.gabor66@gmail.com>
2019-10-19 17:00:20 +0200

    change application config to connect oracle database, created user controller for create /get user by id/ get all user

--- bugtracker/src/main/java/bugtracker/BugTrackerApp.java
+++ bugtracker/src/main/java/bugtracker/BugTrackerApp.java
@@ -2,9 +2,9 @@ package bugtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-
-@SpringBootApplication
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
 public class BugTrackerApp {
 
 	public static void main(String[] args) {
