e057d7589a407a182aee36071c1d1a69dcc48874
Istvan Szollosi <szollosi@gmail.com>
2019-11-30 14:04:47 +0100

    Infinite recursion fix
    (cherry picked from commit 7256153d8e1047564001d2b297de0887631c73e2)

--- bugtracker/src/main/java/bugtracker/SecurityConfig.java
+++ bugtracker/src/main/java/bugtracker/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService())
+		auth.userDetailsService(userService)
 				.passwordEncoder(passwordEncoder());
 	}
 
