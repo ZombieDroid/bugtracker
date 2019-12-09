feeb441c01006f7767e923aeab53b156b57832ed
Istvan Szollosi <szollosi@gmail.com>
2019-12-09 14:15:20 +0100

    New user & user update

--- bugtracker/src/main/resources/static/js/users.js
+++ bugtracker/src/main/resources/static/js/users.js
@@ -1,3 +1,218 @@
+let userdetails = function () {
+    var user = {};
+
+    Ext.Ajax.request({
+        url: '/api/user/'+localStorage.getItem("userId"),
+        method: 'GET',
+        async: false,
+        success: function (form, action) {
+            user = JSON.parse(form.responseText);
+        },
+        failure: function (form, action) {
+            alert(form.responseText);
+        }
+    });
+
+    var getDate = function(date){
+        if(date == null || date < 3){
+            return '01 01 0000';
+        }
+        return date[0] + ' ' + date[1] + ' ' + date[2];
+    };
+
+    var typeStore = Ext.create('Ext.data.Store', {
+        fields: ['type'],
+        data : [
+            {"value":0, "name":"Admin"},
+            {"value":1, "name":"Developer"},
+            {"value":2, "name":"User"},
+            {"value":3, "name":"Approver"}  // TODO: jovahagyo?
+        ]
+    });
+
+    var type = Ext.create('Ext.form.ComboBox', {
+        fieldLabel: 'Type',
+        store: typeStore,
+        queryMode: 'local',
+        displayField: 'name',
+        valueField: 'value'
+    });
+
+
+    var name = Ext.create('Ext.form.TextField', {
+        fieldLabel: 'Name',
+        width: 400,
+        bodyPadding: 10,
+        value: user.name
+    });
+
+    var email = Ext.create('Ext.form.TextField', {
+        fieldLabel: 'Email',
+        width: 400,
+        bodyPadding: 10,
+        value: user.email
+    });
+
+    var password = Ext.create('Ext.form.TextField', {
+        fieldLabel: 'Password',
+        fieldType: 'password',
+        inputType: 'password',
+        width: 400,
+        bodyPadding: 10,
+        value: user.password
+    });
+
+
+    var deleteDate = Ext.create('Ext.form.Date', {
+        fieldLabel: 'Deleted date',
+        width: 400,
+        format: 'Y m d',
+        value: getDate(user.deletedTs)
+    });
+
+    var updateButton = Ext.create('Ext.Button', {
+        text: "Update",
+        style: {
+            border: 1
+        },
+        handler: function () {
+            Ext.Ajax.request({
+                url: "/api/user/modify",
+                method: "POST",
+                jsonData: getUser(),
+                success: function (form, action) {
+                    userdetailswindow.close();
+                    updateUserTable('');
+                },
+                failure: function (form, action) {
+                    alert(form.responseText);
+                }
+            })
+        }
+    });
+
+    var getUser = function(){
+        return {
+            name: name.value,
+            password: password.value,
+            email: email.value,
+            type: type.value
+        }
+    };
+
+    var userdetailswindow = Ext.create('Ext.Window', {
+        width: 1000,
+        height: 500,
+        padding: 15,
+        modal: true,
+        layout: {
+            type: 'vbox',
+            padding: 5
+        },
+        items: [
+            name,
+            password,
+            email,
+            type
+        ],
+        buttons: [
+            updateButton
+        ]
+    }).show();
+};
+
+let newuser = function () {
+    var typeStore = Ext.create('Ext.data.Store', {
+        fields: ['type'],
+        data : [
+            {"value":0, "name":"Admin"},
+            {"value":1, "name":"Developer"},
+            {"value":2, "name":"User"},
+            {"value":3, "name":"Approver"}  // TODO: jovahagyo?
+        ]
+    });
+
+    var userType = Ext.create('Ext.form.ComboBox', {
+        fieldLabel: 'Type',
+        store: typeStore,
+        queryMode: 'local',
+        displayField: 'name',
+        valueField: 'value'
+    });
+
+    var name = Ext.create('Ext.form.TextField', {
+        fieldLabel: 'Name',
+        width: 400,
+        bodyPadding: 10
+    });
+
+    var psw = Ext.create('Ext.form.TextField', {
+        fieldLabel: 'Password',
+        inputType: 'password',
+        width: 400,
+        bodyPadding: 10
+    });
+
+    var email = Ext.create('Ext.form.field.Text', {
+        fieldLabel: 'Email',
+        width: 400,
+        bodyPadding: 10,
+        vtype: 'email'
+    });
+
+    userType.setValue(0);
+
+    var createButton = Ext.create('Ext.Button', {
+        text: "Create",
+        style: {
+            border: 1
+        },
+        handler: function () {
+            Ext.Ajax.request({
+                url: "/api/user/create",
+                method: "POST",
+                jsonData: getUser(),
+                success: function (form, action) {
+                    newuserwindow.close();
+                    updateUserTable('');
+                },
+                failure: function (form, action) {
+                    alert(form.responseText);
+                }
+            })
+        }
+    });
+
+    var getUser = function(){
+        return {
+            name: name.value,
+            password: psw.value,
+            email: email.value,
+            type: userType.getValue()
+        }
+    };
+
+    var newuserwindow = Ext.create('Ext.Window', {
+        width: 1000,
+        height: 500,
+        padding: 15,
+        modal: true,
+        layout: {
+            type: 'vbox',
+            padding: 5
+        },
+        items: [
+            name,
+            psw,
+            email,
+            userType
+        ],
+        buttons: [
+            createButton
+        ]
+    }).show();
+};
+
 let users = {};
 
 let userStore = Ext.create('Ext.data.Store', {
@@ -48,7 +263,7 @@ Ext.define('App.view.UserPanel', {
         listeners : {
             itemdblclick : function(view, cell, cellIndex, record, row, rowIndex, e) {
                 localStorage.setItem("userId", cell.data.id);
-                window.location.assign("/user/modify");
+                userdetails();
             }
         }
     }
@@ -59,9 +274,7 @@ let userPanel = Ext.create('App.view.UserPanel', {
 
 let newUserButton = Ext.create('Ext.button.Button', {
     text: 'New user',
-    handler: function(){
-        window.location.assign("/user/new");
-    }
+    handler: newuser
 });
 
 let searchUserField = Ext.create('Ext.form.TextField', {
