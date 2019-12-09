f2fcdff5be84ddfaf8899732c064ef61da5eaa1f
Istvan Szollosi <szollosi@gmail.com>
2019-12-09 02:15:23 +0100

    Project details dialog

--- bugtracker/src/main/resources/static/js/projects.js
+++ bugtracker/src/main/resources/static/js/projects.js
@@ -1,3 +1,172 @@
+let projectdetails = function() {
+    var project = {};
+
+    Ext.Ajax.request({
+        url: '/api/project/' + localStorage.getItem("projectId"),
+        method: 'GET',
+        async: false,
+        success: function (form, action) {
+            project = JSON.parse(form.responseText);
+        },
+        failure: function (form, action) {
+            alert(form.responseText);
+        }
+    });
+
+    var getDate = function (date) {
+        if (date == null || date < 3) {
+            return '01 01 0000';
+        }
+        return date[0] + ' ' + date[1] + ' ' + date[2];
+    };
+
+    var userStore = Ext.create('Ext.data.Store', {
+        fields: ['id', 'name']
+    });
+
+    var devStore = Ext.create('Ext.data.Store', {
+        fields: ['id', 'name']
+    });
+
+    var approver = Ext.create('Ext.form.ComboBox', {
+        fieldLabel: 'Approver',
+        store: userStore,
+        queryMode: 'local',
+        displayField: 'name',
+        valueField: 'id'
+    });
+
+    var devs = Ext.create('Ext.form.ComboBox', {
+        fieldLabel: 'Developer',
+        store: devStore,
+        queryMode: 'local',
+        displayField: 'name',
+        valueField: 'id'
+    });
+
+    var name = Ext.create('Ext.form.TextField', {
+        fieldLabel: 'Name',
+        width: 400,
+        bodyPadding: 10,
+        value: project.name
+    });
+
+    var desc = Ext.create('Ext.form.TextArea', {
+        fieldLabel: 'Description',
+        width: 400,
+        bodyPadding: 10,
+        value: project.description
+    });
+
+    var s1date = Ext.create('Ext.form.Date', {
+        fieldLabel: 'S1 date',
+        width: 400,
+        format: 'Y m d',
+        value: getDate(project.s1Time)
+    });
+
+    var s2date = Ext.create('Ext.form.Date', {
+        fieldLabel: 'S2 date',
+        width: 400,
+        format: 'Y m d',
+        value: getDate(project.s2Time)
+    });
+
+    var s3date = Ext.create('Ext.form.Date', {
+        fieldLabel: 'S3 date',
+        width: 400,
+        format: 'Y m d',
+        value: getDate(project.s3Time)
+    });
+
+    Ext.Ajax.request({
+        url: '/api/user/getAllApprover',
+        method: 'GET',
+        success: function (form, action) {
+            var allUsers = JSON.parse(form.responseText);
+            for (var i = 0; i < allUsers.length; i++) {
+                userStore.add({id: allUsers[i].id, name: allUsers[i].name});
+            }
+
+            approver.setValue(project.defaultApproverId);
+        },
+        failure: function (form, action) {
+            alert(form.responseText);
+        }
+    });
+
+    Ext.Ajax.request({
+        url: '/api/user/getAllDeveloper',
+        method: 'GET',
+        success: function (form, action) {
+            var allDev = JSON.parse(form.responseText);
+            for (var i = 0; i < allDev.length; i++) {
+                devStore.add({id: allDev[i].id, name: allDev[i].name});
+            }
+
+            devs.setValue(project.defaultDeveloperId);
+        },
+        failure: function (form, action) {
+            alert(form.responseText);
+        }
+    });
+
+    var updateButton = Ext.create('Ext.button.Button', {
+        text: 'Update',
+        handler: function () {
+            Ext.Ajax.request({
+                url: '/api/project/update',
+                method: 'POST',
+                jsonData: getProject(),
+                success: function (form, action) {
+                    this.close();
+                    // window.location.assign("/project/all");
+                },
+                failure: function (form, action) {
+                    this.close();
+                    // alert(form.responseText);
+                }
+            });
+        }
+    });
+
+    var getProject = function () {
+        return {
+            id: project.id,
+            name: name.value,
+            description: desc.value,
+            defaultApproverId: approver.value,
+            defaultDeveloperId: devs.value,
+            s1Time: s1date.value,
+            s2Time: s2date.value,
+            s3Time: s3date.value
+        }
+    };
+
+    Ext.create('Ext.Window', {
+        width: 1000,
+        height: 500,
+        padding: 15,
+        modal: true,
+        layout: {
+            type: 'vbox',
+            padding: 5
+        },
+        items: [
+            name,
+            desc,
+            approver,
+            devs,
+            s1date,
+            s2date,
+            s3date
+        ],
+        buttons: [
+            updateButton
+        ]
+    }).show();
+};
+
 let projects = {};
 
 let projectStore = Ext.create('Ext.data.Store', {
@@ -84,7 +253,7 @@ Ext.define('BugtrackerApp.view.ProjectPanel', {
         listeners : {
             itemdblclick : function(view, cell, cellIndex, record, row, rowIndex, e) {
                 localStorage.setItem("projectId", cell.data.id);
-                window.location.assign("/project");
+                projectdetails();
             }
         }
     },
@@ -97,15 +266,11 @@ Ext.define('BugtrackerApp.view.ProjectPanel', {
 });
 
 let projectPanel = Ext.create('BugtrackerApp.view.ProjectPanel', {
-    // renderTo: "projectlistdiv"
 });
 
 let newButton = Ext.create('Ext.button.Button', {
     text: 'New',
     handler: newproject
-    // handler: function(){
-    //     window.location.assign("/project/new");
-    // }
 });
 
 let updateProjectTable = function(searchText){
