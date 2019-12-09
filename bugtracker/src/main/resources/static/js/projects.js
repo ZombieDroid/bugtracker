af69a2f3c6fea46e6decfde44736c3f2830b1ec1
Istvan Szollosi <szollosi@gmail.com>
2019-12-09 03:25:10 +0100

    Moved all project related functions to projects.js

--- bugtracker/src/main/resources/static/js/projects.js
+++ bugtracker/src/main/resources/static/js/projects.js
@@ -167,6 +167,148 @@ let projectdetails = function() {
     }).show();
 };
 
+let newproject = function () {
+
+    var userStore = Ext.create('Ext.data.Store', {
+        fields: ['id', 'name']
+    });
+
+    var devStore = Ext.create('Ext.data.Store', {
+        fields: ['id', 'name']
+    });
+
+    var approver = Ext.create('Ext.form.ComboBox', {
+        fieldLabel: 'Approver',
+        store: userStore,
+        queryMode: 'local',
+        displayField: 'name',
+        valueField: 'id'
+    });
+
+    var devs = Ext.create('Ext.form.ComboBox', {
+        fieldLabel: 'Developer',
+        store: devStore,
+        queryMode: 'local',
+        displayField: 'name',
+        valueField: 'id'
+    });
+
+    var name = Ext.create('Ext.form.TextField', {
+        fieldLabel: 'Name',
+        width: 400,
+        bodyPadding: 10
+    });
+
+    var desc = Ext.create('Ext.form.TextArea', {
+        fieldLabel: 'Description',
+        width: 400,
+        bodyPadding: 10
+    });
+
+    var s1date = Ext.create('Ext.form.Date', {
+        fieldLabel: 'S1 date',
+        width: 400,
+        value: new Date()
+    });
+
+    var s2date = Ext.create('Ext.form.Date', {
+        fieldLabel: 'S2 date',
+        width: 400,
+        value: new Date()
+    });
+
+    var s3date = Ext.create('Ext.form.Date', {
+        fieldLabel: 'S3 date',
+        width: 400,
+        value: new Date()
+    });
+
+    var createButton = Ext.create('Ext.Button', {
+        text: "Create",
+        style: {
+            border: 1
+        },
+        handler: function () {
+            Ext.Ajax.request({
+                url: "/api/project/create",
+                method: "POST",
+                jsonData: getProject(),
+                success: function (form, action) {
+                    window.location.assign("/project/all");
+                },
+                failure: function (form, action) {
+                    alert(form.responseText);
+                }
+            })
+        }
+    });
+
+    var getProject = function(){
+        return {
+            name: name.value,
+            description: desc.value,
+            defaultApproverId: approver.value,
+            defaultDeveloperId: devs.value,
+            s1Time: s1date.value,
+            s2Time: s2date.value,
+            s3Time: s3date.value
+        }
+    };
+
+    Ext.create('Ext.Window', {
+        width: 640,
+        height: 480,
+        padding: 15,
+        title: 'New project',
+        modal: true,
+        items: [
+            name,
+            desc,
+            approver,
+            devs,
+            s1date,
+            s2date,
+            s3date
+        ],
+        buttons: [
+            createButton
+        ]
+    }).show();
+
+    Ext.Ajax.request({
+        url: '/api/user/getAllApprover',
+        method: 'GET',
+        success: function (form, action) {
+            var allUsers = JSON.parse(form.responseText);
+            for(var i=0; i<allUsers.length; i++){
+                userStore.add({id: allUsers[i].id, name: allUsers[i].name});
+            }
+
+            approver.setValue(allUsers[0].id);
+        },
+        failure: function (form, action) {
+            alert(form.responseText);
+        }
+    });
+
+    Ext.Ajax.request({
+        url: '/api/user/getAllDeveloper',
+        method: 'GET',
+        success: function (form, action) {
+            var allDev = JSON.parse(form.responseText);
+            for(var i=0; i<allDev.length; i++){
+                devStore.add({id: allDev[i].id, name: allDev[i].name});
+            }
+
+            devs.setValue(allDev[0].id);
+        },
+        failure: function (form, action) {
+            alert(form.responseText);
+        }
+    });
+};
+
+
 let projects = {};
 
 let projectStore = Ext.create('Ext.data.Store', {
