34b3242c7713bbd5ebc6e5f29577040bec4c9647
Istvan Szollosi <szollosi@gmail.com>
2019-12-15 23:07:19 +0100

    Comment backend stuff

---
+++ bugtracker/src/main/java/bugtracker/comment/CommentEntity.java
@@ -0,0 +1,61 @@
+package bugtracker.comment;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "COMMENT")
+public class CommentEntity {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private long ticketId;
+    private long userId;
+
+    private String commentText;
+    private LocalDateTime commentTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getTicketId() {
+        return ticketId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public LocalDateTime getCommentTime() {
+        return commentTime;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTicketId(long ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public void setCommentTime(LocalDateTime commentTime) {
+        this.commentTime = commentTime;
+    }
+}
